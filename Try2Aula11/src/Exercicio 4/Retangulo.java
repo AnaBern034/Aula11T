a81c128f1ed95d22184ca75813c15d5f0ae1865c
ana <ana.bernardes@zup.com.br>
2023-04-26 15:37:01 -0300

    primeiro commit

---
+++ Try2Aula11/src/Exercicio 4/Retangulo.java
@@ -0,0 +1,8 @@
+Algoritmo "Base do retangulodo"
+double base, altura, area
+double.parseDouble(Base)
+        double.parseDouble(altura)
+area = base * altura
+system.out.println ("A area do retangulo é",area)
+
+        .
---
+++ Try2Aula11/src/Exercicio 7/MaiorDeIdade.java
@@ -0,0 +1,5 @@
+Algoritmo "Maior de idade"
+int X
+if X > 18
+system.out.println("Você é maior de idade")
+        .
